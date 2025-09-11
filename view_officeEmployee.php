828c321fadf8004c2a16b9c5647d1d41e587febe
Eileen <ecynthia01@student.ciputra.ac.id>
2025-09-12 02:38:55 +0700

    officeEmployee

---
+++ view_officeEmployee.php
@@ -0,0 +1,75 @@
+<?php require("controller_employee.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>OfficeEmployee</title>
+</head>
+
+    <body class="container my-5">
+    <div class="container">
+        <div class="container">
+            <div class="card text-center">
+                <div class="card-header">
+                          <ul class="nav nav-tabs card-header-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="true" href="view_addemployee.php">Tambah Karyawan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="view_employee.php">List Karyawan</a>
+                        </li>
+                           <li class="nav-item">
+                            <a class="nav-link" href="view_addOffice.php">Tambah Office</a>
+                        </li>
+                      
+                         <li class="nav-item">
+                            <a class="nav-link" href="view_office.php">List Office</a>
+                        </li>
+                          <li class="nav-item">
+                            <a class="nav-link active" href="view_officeEmployee.php">Office Employee</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                      <h1 class="mb-4">Employee Office</h1>
+                      <table class="table table-bordered">
+                        <thead class="table-info">
+                            <tr>
+                                <th scope="col">Employee</th>
+                                <th scope="col">Office</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                         <tbody>
+                            <?php
+                            $allEmployee = getAllEmployees();
+                            foreach ($allEmployee as $index => $employee) {
+                           ?>
+                                <tr>
+                                    <td><?=$employee->nama?></td>
+                                    <td><?=isset($employee->office_id) && $employee->office_id ? $employee->office_id : 'N/A'?></td>
+                                   <td>
+                                        <a href="controller_employee.php?deleteID=<?= $index ?>">
+                                            <button class="btn btn-danger">Delete</button>
+                                        </a>
+                                    </td>
+                               
+                                </tr>
+                            <?php
+                            }
+                            ?>
+
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
